70564e3d24d3a70289c5a4de10187c6db24ccffb
renedeanda <rene.ideanda@gmail.com>
2021-04-11 15:53:59 -0500

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -86,21 +86,23 @@ export default function Home({ companies }: { companies: any[] }) {
                   <div style={{ color: '#0C5FFF' }}>Open-sourced</div>
                 </Header>
                 <Button
+                  as='a'
                   style={{ display: 'inline-block', margin: '0.3em' }}
                   color='purple'
                   icon='google'
                   content='Add Company'
                   href='https://forms.gle/Y75CegGFRp6tzdn67'
-                  target='_blank' 
-                  rel="noopener"/>
+                  target='_blank'
+                  rel="noopener" />
                 <Button
+                  as='a'
                   style={{ display: 'inline-block', margin: '0.3em' }}
                   className='github-button'
                   icon='github'
                   content='GitHub'
                   href='https://github.com/renedeanda/Tech.Viet'
-                  target='_blank' 
-                  rel="noopener"/>
+                  target='_blank'
+                  rel="noopener" />
               </Grid.Column>
             </Grid.Row>
             <Grid.Row style={{ padding: 0, margin: 0 }}>
