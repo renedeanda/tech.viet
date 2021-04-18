fd876682e4800772a977126e9fc05bec2bac680e
renedeanda <rene.ideanda@gmail.com>
2021-04-17 22:40:12 -0500

    Open companies in modals

---
+++ components/companyModal.tsx
@@ -0,0 +1,21 @@
+import { Modal } from 'semantic-ui-react';
+import { Company } from '../types/company.types';
+import CompanyContainer from './companyContainer';
+
+export default function CompanyModal({ company, open, onClose }: {
+  company: Company,
+  open: boolean,
+  onClose: any
+}) {
+
+  return (
+    <Modal
+      basic
+      open={open}
+      closeOnDimmerClick={true}
+      onClose={onClose}
+      closeIcon>
+      <CompanyContainer company={company} />
+    </Modal>
+  )
+}
