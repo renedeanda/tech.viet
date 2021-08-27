eb40c5c83a3d387266056c7c9d053e6913f7aeec
renedeanda <rene.ideanda@gmail.com>
2021-08-26 18:31:27 -0700

    update Twitter share text

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -14,8 +14,6 @@ export default function Meta(props: SEOProps) {
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={props.title} />
       <meta name="twitter:description" content={props.desc} />
-      <meta name="twitter:site" content="@redeio" />
-      <meta name="twitter:creator" content="@redeio" />
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="HandheldFriendly" content="true" />
