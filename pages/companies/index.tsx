d1b1dbc0d0e7c47d127d7062a0b8b9512e43f116
renedeanda <rene.ideanda@gmail.com>
2022-03-28 13:43:13 -0700

    Update metadata

--- pages/companies/index.tsx
+++ pages/companies/index.tsx
@@ -90,7 +90,7 @@ export default function Home({ companies }: { companies: any[] }) {
   return (
     <div>
       <Meta
-        title='Viet.io - Vietnam Startup Ecosystem'
+        title='Viet.io - Vietnam Companies'
         desc='List of 200+ Vietnam startups and big tech companies. Viet.io is an open-source website built with React and Next.js listing 200+ technology companies in Vietnam.'
         canonical='https://viet.io/companies' />
 
