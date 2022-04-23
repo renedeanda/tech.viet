60feb7efd39909fc4720bccaaebb2f60f9d5c90b
renedeanda <rene.ideanda@gmail.com>
2022-04-23 08:06:24 -0700

    Update logoUrl with local file reference for co json files

--- components/companyContainer.tsx
+++ components/companyContainer.tsx
@@ -18,7 +18,7 @@ export default function CompanyContainer({ company, modal }: {
 }) {
 
   // Load local image file if exists
-  const avatarSrc = company.facebook ? `/img/company/${company.slug}-avatar.png` : '/company.png'
+  const avatarSrc = company.logoUrl ? company.logoUrl : '/company.png'
 
   const hiringText = company.hiring ? "Yes" : "No";
   const gFormLink = `https://docs.google.com/forms/d/e/1FAIpQLSelgDTevZ0xCrTv9SsWnlpE-vw4gofE-2s-c_tKaYo7HJwVUw/viewform?usp=pp_url&entry.2005620554=${company.name}&entry.1692157935=${company.website}&entry.1045781291=${company.industry}&entry.1065046570=${company.tagline}&entry.564514234=${company.logoUrl}&entry.171074559=${company.description}&entry.1166974658=${company.facebook}&entry.361763259=${company.linkedin}&entry.839337160=${company.blogUrl}&entry.2015302511=${company.androidUrl}&entry.302652646=${company.iosUrl}&entry.1200097343=${company.demoUrl}&entry.190791171=${hiringText}`;
--- scripts/python-scripts/json-updates.py
+++ scripts/python-scripts/json-updates.py
@@ -1,6 +1,8 @@
 import os
 import json
 from slugify import slugify
+from glob import glob  # for reading folder of json files
+
 
 def create_investor_json_files():
     file = '../../public/data/vcv_json.json'
@@ -34,7 +36,82 @@ def create_investor_json_files():
                         json.dump(data, f, ensure_ascii=False)
                 except:
                     continue
-
     return
 
-create_investor_json_files()
+
+def update_inv_json():
+    investors = read_inv_json_files()
+    imgPath = os.path.abspath("../../public/img/investor")
+    jsonPath = os.path.abspath("../../public/data/investors")
+
+    for inv in investors:
+        slug = inv["slug"]
+        if os.path.isfile(f'{imgPath}/{slug}-avatar.png'):
+            # skips creating image if file exists
+            print(f'Image exists - {slug}')
+            try:
+                inv["logoUrl"] = f'/img/investor/{slug}-avatar.png'
+                print(f'/img/investor/{slug}.json')
+                print(inv)
+                with open(f'{jsonPath}/{slug}.json', 'w') as f:
+                    json.dump(inv, f, ensure_ascii=False)
+            except:
+                continue
+        else:
+            try:
+                print(f'No image exists - {slug}')
+            except:
+                continue
+    return
+
+
+def update_co_json():
+    companies = read_json_files()
+    imgPath = os.path.abspath("../../public/img/company")
+    jsonPath = os.path.abspath("../../public/data/companies")
+
+    for co in companies:
+        slug = co["slug"]
+        if os.path.isfile(f'{imgPath}/{slug}-avatar.png'):
+            # skips creating image if file exists
+            print(f'Image exists - {slug}')
+            try:
+                co["logoUrl"] = f'/img/company/{slug}-avatar.png'
+                print(f'/img/company/{slug}.json')
+                print(co)
+                with open(f'{jsonPath}/{slug}.json', 'w') as f:
+                    json.dump(co, f, ensure_ascii=False)
+            except:
+                continue
+        else:
+            try:
+                print(f'No image exists - {slug}')
+            except:
+                continue
+    return
+
+
+def read_json_files():
+    companies = []
+    files = glob('../../public/data/companies/*', recursive=True)
+
+    # Loop through files
+    for single_file in files:
+        with open(single_file, 'r') as f:
+            json_file = json.load(f)
+            companies.append(json_file)
+    return companies
+
+def read_inv_json_files():
+    investors = []
+    files = glob('../../public/data/investors/*', recursive=True)
+
+    # Loop through files
+    for single_file in files:
+        with open(single_file, 'r') as f:
+            json_file = json.load(f)
+            investors.append(json_file)
+    return investors
+
+# create_investor_json_files()
+update_co_json()
