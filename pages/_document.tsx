2110c0224f75f747cf8b419ab186c469d755d3e5
renedeanda <rene.ideanda@gmail.com>
2024-01-04 23:01:18 -0800

    Update links & add BMAC widget

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="rede" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
         </Head>
         <body>
           <Main />
